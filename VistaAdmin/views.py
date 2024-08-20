88d45abe7a45a9efcf1d49fed153f819e3800386
AntonioC2421 <cotrina210101@gmail.com>
2024-08-20 18:28:48 -0400

    Creacion de ViewCamisetas

--- VistaAdmin/views.py
+++ VistaAdmin/views.py
@@ -2,5 +2,4 @@ from django.shortcuts import render
 from .models import Categorias
 #Pagina Principal de el Admin
 def MainPrincipal(request):
-    
-    return render(request, './TemplatesAdmin/Principl/Principal.html')
+    return render(request, './TemplatesAdmin/Principal/Principal.html')
--- VistaCliente/urls.py
+++ VistaCliente/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 app_name = 'ViewsClient'
 urlpatterns = [
     path('',views.MainPrincipalCliente, name='MainPrincipalCliente'),
-    path('ViewCamisetas/<int:id>', views.ViewCamisetas, name='ViewsCamiset')
+    path('ViewCamisetas/<int:id>', views.ViewCamisetas, name='ViewsCamiset'),
 ]
