42939fc98e79e11ffa5111ff08a08245bfb7ee19
AntonioC2421 <cotrina210101@gmail.com>
2024-08-21 16:14:06 -0400

    Cambio en Views ViewCamisetas

--- VistaCliente/views.py
+++ VistaCliente/views.py
@@ -7,9 +7,25 @@ def MainPrincipalCliente(request):
     data = {'category': Category, 'equipos': Equipo}
     return render(request, './TemplatesClientes/MainCliente/IndexCliente.html',data)
 
-def ViewCamisetas(request,id):
+def ViewCamisetas(request, id=None, team_id=None):
     Category = Categorias.objects.all()
-    SubCategory = SubCategoria.objects.filter(id_CategoriasCamisetas = id)
-    Equipo = Teams.objects.all()
-    data = {'equi': SubCategory,'category': Category, 'camis': Equipo}
+    SubCategory = SubCategoria.objects.filter(id_CategoriasCamisetas=id)
+    Equipo = Teams.objects.filter(id_SubCategoria__in=SubCategory)
+
+    if team_id:
+        Equipo = Equipo.filter(id=team_id)
+
+    encontrado = False
+    for cam in Equipo:
+        if cam.id_SubCategoria.id_CategoriasCamisetas.id == id:
+            encontrado = True
+            break
+
+    data = {
+        'equi': SubCategory,
+        'category': Category,
+        'camis': Equipo,
+        'encontrado': encontrado,
+        'idn': id,
+    }
     return render(request, './TemplatesClientes/ViewCamisetas/ViewCamisetas.html', data)
