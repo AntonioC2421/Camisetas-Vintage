7c6a40a46541be355f4d0e04f245e8c22f3aa6ba
AntonioC2421 <cotrina210101@gmail.com>
2024-08-28 15:25:16 -0400

    Cambio de diseño en Principal.html (vista del admin)

--- VistaCliente/views.py
+++ VistaCliente/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from VistaAdmin.models import Categorias, SubCategoria,Teams,Size,TeamsImgs,Marca,CodigoPromocional
+from django.core.serializers import serialize
 
 def MainPrincipalCliente(request):
     Equipo = Teams.objects.all()
-    data = {'equipos': Equipo}
+    imgTeam = TeamsImgs.objects.all()
+    imgTeam_serialized = serialize('json', imgTeam)
+    
+    data = {'equipos': Equipo,'imgTeam': imgTeam_serialized}
+    
     return render(request, './TemplatesClientes/MainCliente/IndexCliente.html',data)
 
 def ViewCamisetas(request, id=None, team_id=None):
     SubCategory = SubCategoria.objects.filter(id_CategoriasCamisetas=id) #Real , Barcelona ... (Muestra los equipos de la Liga seleccionada)
     Equipo = Teams.objects.filter(id_SubCategoria__in=SubCategory) #Camiseta Real madrid 2018 (más img) | (Muetra las camisetas (img/info) del equipo seleccionado)
-
+    
     if team_id:
         Equipo = Equipo.filter(id_SubCategoria=team_id) #Seleccionar una equipo y mostrar solo las camisetas de ese quipo
 
@@ -27,7 +32,5 @@ def ViewCamisetas(request, id=None, team_id=None):
 def DetalleCamiseta(request, id):
     DetallesImG = TeamsImgs.objects.filter(teams = id)
     Detalle = Teams.objects.filter(id = id)
-    data1 = {'imgs':DetallesImG}
-    data3 = {"deta" : Detalle}
-    data = {**data1,**data3}
+    data = {'imgs':DetallesImG, "deta" : Detalle}
     return render(request, './TemplatesClientes/DetalleCamiseta/DetalleCamiseta.html',data)
--- static/js/IndexCliente.js
+++ static/js/IndexCliente.js
@@ -1,11 +1,11 @@
-const prev=document.querySelector('.prev')
-        const next=document.querySelector('.next')
-        const slider=document.querySelector('.slider')
+const prev = document.querySelector('.prev')
+const next = document.querySelector('.next')
+const slider = document.querySelector('.slider')
 
-        prev.addEventListener('click', ()=>{
-            slider.scrollLeft -=300
-        })
+prev.addEventListener('click', () => {
+    slider.scrollLeft -= 300
+})
 
-        next.addEventListener('click', ()=>{
-            slider.scrollLeft +=300
-        })
+next.addEventListener('click', () => {
+    slider.scrollLeft += 300
+})
